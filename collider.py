fd57033221cbf5df0fa5d57bd5fc8dfb01a71e72
Supermango407 <150284897+Supermango407@users.noreply.github.com>
2025-10-11 19:24:43 -0500

    added rect collider

--- collider.py
+++ collider.py
@@ -7,9 +7,11 @@ from gameobject import Gameobject
 
 class ColliderTypes(Enum):
     CIRCLE = 1
+    RECT = 2
 
 
 class Collider(Gameobject):
+    """a class for dectecting overlapping events."""
 
     def __init__(self, position, parrent=None, hidden=True):
         super().__init__(position, parrent, hidden)
@@ -23,21 +25,26 @@ class Collider(Gameobject):
         return False
 
     def mouse_over(self) -> bool:
-        return self.point_over(pygame.mouse.get_pos())
+        return self.point_over(Vector2(pygame.mouse.get_pos()))
 
 
 class CircleCollider(Collider):
 
-    def __init__(self, position=Vector2(0, 0), radius=None, border_width:float=2, parrent=None, hidden=True):
+    def __init__(self, position=Vector2(0, 0), radius:float=None, border_width:float=2, parrent:Gameobject=None, hidden:bool=True):
+        """
+        raduis: the size of the collier.
+            if raduis is None will default to parrents radius
+        """
         super().__init__(position, parrent, hidden)
         if radius != None:
             self.radius = radius
         else:
             self.radius = parrent.radius
+        
         self.border_width = border_width
 
-    def point_over(self, pos):
-        return (pos-self.global_position()).magnitude() <= self.radius
+    def point_over(self, point:Vector2):
+        return (point-self.global_position()).magnitude() <= self.radius
 
     def draw(self):
         color = (255, 0, 0) if self.mouse_over() else (0, 255, 0)
@@ -47,7 +54,35 @@ class CircleCollider(Collider):
         pygame.draw.line(Gameobject.window, color, Vector2(pos.x-self.radius, pos.y), Vector2(pos.x+self.radius, pos.y), self.border_width)
 
 
+class RectCollider(Collider):
+
+    def __init__(self, position=Vector2(0, 0), size:list[float, float]=None, border_width:float=2, parrent:Gameobject=None, hidden:bool=True):
+        """
+        size: the size of the collier.
+            if size is None will default to parrents size
+        """
+        super().__init__(position, parrent, hidden)
+        if size != None:
+            self.size = size
+        else:
+            self.size = parrent.size
+
+        self.border_width = border_width
+
+    def point_over(self, point:Vector2):
+        pos = self.global_position()
+        return pos.x < point.x < pos.x+self.size[0] and pos.y < point.y < pos.y+self.size[1]
+
+    def draw(self):
+        color = (255, 0, 0) if self.mouse_over() else (0, 255, 0)
+        pos = self.global_position()
+        pygame.draw.rect(Gameobject.window, color, (pos.x, pos.y, self.size[0], self.size[1]), self.border_width)
+        pygame.draw.line(Gameobject.window, color, Vector2(pos.x+self.border_width, pos.y+self.border_width), Vector2(pos.x+self.size[0]-self.border_width, pos.y+self.size[1]-self.border_width), self.border_width)
+        pygame.draw.line(Gameobject.window, color, Vector2(pos.x+self.border_width, pos.y+self.size[1]-self.border_width), Vector2(pos.x+self.size[0]-self.border_width, pos.y+self.border_width), self.border_width)
+
+
 collider_classes = {
-    ColliderTypes.CIRCLE: CircleCollider
+    ColliderTypes.CIRCLE: CircleCollider,
+    ColliderTypes.RECT: RectCollider,
 }
 
--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import pygame
 from pygame import Vector2
 import os
 from gameobject import Gameobject
-from collider import CircleCollider
+from collider import CircleCollider, RectCollider
 
 
 class Circle(Gameobject):
@@ -15,13 +15,29 @@ class Circle(Gameobject):
         super().__init__(position)
         self.radius = radius
         self.color = color
-        self.collider = CircleCollider(parrent=self, hidden=False, border_width=2)
+        self.collider = CircleCollider(parrent=self, hidden=False)
 
     def draw(self):
         pygame.draw.circle(Gameobject.window, self.color, self.global_position(), self.radius)
 
 
+class Rect(Gameobject):
+    def __init__(self, position, size:list[float, float], color:tuple[int, int, int]):
+        """
+            color: the color of the circle.
+        """
+        super().__init__(position)
+        self.size = size
+        self.color = color
+        self.collider = RectCollider(parrent=self, hidden=False)
+
+    def draw(self):
+        pos = self.global_position()
+        pygame.draw.rect(Gameobject.window, self.color, (pos.x, pos.y, self.size[0], self.size[1]))
+
+
 circle = Circle(Vector2(150, 100), 50, (255, 255, 255))
+rect = Rect(Vector2(250, 250), [150, 100], (255, 255, 255))
 
 
 if __name__ == '__main__':
